b2fe4fc43add5c85f03f47a3ea24b62f8048c732
Matthew Petersen <matthew_petersen@brown.edu>
2016-11-27 16:12:55 -0500

    callback prototype

--- GUI/src/fem_b_gui.f
+++ GUI/src/fem_b_gui.f
@@ -2,108 +2,225 @@
 version 1.0304
 header_name {.h}
 code_name {.cxx}
+decl {\#include <iostream>} {selected private local
+}
+
 Function {make_window()} {open
 } {
-  Fl_Window {} {
+  Fl_Window main_window {
     label {Laminar Combustion Input File Generator} open
-    xywh {1215 270 500 700} type Double visible
+    xywh {1202 70 678 749} type Double visible
   } {
-    Fl_Group {} {
-      label {Wall Settings} open
-      xywh {15 30 475 475}
+    Fl_Group bc_set {
+      label {Boundary Conditions} open
+      xywh {190 30 475 475}
     } {
-      Fl_Round_Button {} {
+      Fl_Round_Button iso_button {
         label Isothermal
-        xywh {15 40 25 25} down_box ROUND_DOWN_BOX
+        xywh {510 120 25 25} down_box ROUND_DOWN_BOX align 4
       }
-      Fl_Round_Button {} {
+      Fl_Round_Button adia_button {
         label Adiabatic
-        xywh {15 60 25 25} down_box ROUND_DOWN_BOX
+        xywh {510 140 25 25} down_box ROUND_DOWN_BOX align 4
       }
-      Fl_Input {} {
-        label {Wall Temperature} selected
-        xywh {335 45 130 25}
+      Fl_Input wall_temp {
+        label {Wall Temperature}
+        xywh {510 45 130 25}
       }
-      Fl_Input {} {
+      Fl_Input in_temp {
         label {Inlet Temperature}
-        xywh {335 70 130 25}
+        xywh {510 70 130 25}
       }
-      Fl_Input {} {
-        label {Inlet Pressure}
-        xywh {335 95 130 25}
-      }
-      Fl_Input {} {
+      Fl_Input out_p {
         label {Outlet Pressure}
-        xywh {335 120 130 25}
+        xywh {510 95 130 25}
       }
     }
-    Fl_Group {} {
+    Fl_Group gas_set {
       label {Gas Settings} open
-      xywh {10 186 475 473}
+      xywh {185 186 475 473}
     } {
-      Fl_Input {} {
+      Fl_Input gas_tc {
         label {Thermal Conductivity}
-        xywh {335 211 130 25}
+        xywh {510 211 130 25}
       }
-      Fl_Input {} {
+      Fl_Input gas_dens {
         label Density
-        xywh {335 235 130 25}
+        xywh {510 235 130 25}
       }
-      Fl_Input {} {
+      Fl_Input gas_sh {
         label {Specific Heat}
-        xywh {335 260 130 25}
+        xywh {510 260 130 25}
       }
-      Fl_Input {} {
+      Fl_Input gas_visc {
         label Viscosity
-        xywh {335 285 130 25}
+        xywh {510 285 130 25}
       }
-      Fl_Input {} {
+      Fl_Input gas_mf {
         label {Percent Fuel}
-        xywh {335 310 130 25}
+        xywh {510 310 130 25}
       }
     }
-    Fl_Group {} {
+    Fl_Group chem_set {
       label {Chemistry Settings} open
-      xywh {10 363 475 477}
+      xywh {185 363 475 477}
     } {
-      Fl_Input {} {
+      Fl_Input rx_rate {
         label {Reaction Rate}
-        xywh {335 393 130 25}
+        xywh {510 393 130 25}
       }
-      Fl_Input {} {
+      Fl_Input fuel_h {
         label {Enthalpy of Fuel}
-        xywh {335 415 130 25}
+        xywh {510 415 130 25}
       }
     }
-    Fl_Group {} {
+    Fl_Group mesh_set {
       label {Mesh Settings} open
-      xywh {10 475 475 475}
+      xywh {185 475 475 475}
     } {
-      Fl_Input {} {
-        label {User File}
-        xywh {335 500 130 25}
-      }
-      Fl_Input {} {
+      Fl_Input x_elem {
         label {Elements (X)}
-        xywh {335 525 130 25}
+        xywh {510 490 130 25}
       }
-      Fl_Input {} {
+      Fl_Input y_elem {
         label {Elements (Y)}
-        xywh {335 550 130 25}
+        xywh {510 515 130 25}
       }
     }
-    Fl_Group {} {
+    Fl_Group time_set {
       label {Time Settings} open
-      xywh {10 610 475 675}
+      xywh {185 575 475 675}
     } {
-      Fl_Input {} {
+      Fl_Input duration {
         label Duration
-        xywh {335 635 130 25}
+        xywh {510 600 130 25}
       }
-      Fl_Input {} {
+      Fl_Input timestep {
         label Timestep
-        xywh {335 660 130 25}
+        xywh {510 625 130 25}
       }
+    }
+    Fl_Button submit {
+      label Submit
+      callback {char* Tw_str, Ti_str, TC_str, dens_str, SH_str, visc_str, MF_str; //Po_str
+char* RX_str, Hfuel_str, El_x_str, El_y_str, dur_str, timestep_str;
+char* input_str, mesh_str;
+bool adia_iso;
+
+if (wall_temp->value()) {
+strcpy(Tw_str, wall_temp->value());
+} else {
+strcpy(Tw_str,"300");
+}
+
+if (in_temp->value()) {
+strcpy(Ti_str, in_temp->value());
+} else {
+strcpy(Ti_str,"300");
+}
+
+// if (out_p->value()) {
+// strcpy(Po_str, out_p->value());
+// } else {
+// strcpy(Po_str,"300");
+// }
+
+if (gas_tc->value()) {
+strcpy(TC_str, gas_tc->value());
+} else {
+strcpy(TC_str,"300");
+}
+
+if (gas_dens->value()) {
+strcpy(dens_str, gas_dens->value());
+} else {
+strcpy(dens_str,"300");
+}
+
+if (gas_sh->value()) {
+strcpy(SH_str, gas_sh->value());
+} else {
+strcpy(SH_str,"300");
+}
+
+if (gas_visc->value()) {
+strcpy(visc_str, gas_visc->value());
+} else {
+strcpy(visc_str,"300");
+}
+
+if (gas_mf->value()) {
+strcpy(MF_str, gas_mf->value());
+} else {
+strcpy(MF_str,"300");
+}
+
+if (rx_rate->value()) {
+strcpy(RX_str, rx_rate->value());
+} else {
+strcpy(RX_str,"300");
+}
+
+if (fuel_h->value()) {
+strcpy(Hfuel_str, fuel_h->value());
+} else {
+strcpy(Hfuel_str,"300");
+}
+
+if (x_elem->value()) {
+strcpy(El_x_str, x_elem->value());
+} else {
+strcpy(El_x_str,"300");
+}
+
+if (y_elem->value()) {
+strcpy(El_y_str, y_elem->value());
+} else {
+strcpy(El_x_str,"300");
+}
+
+if (duration->value()) {
+strcpy(dur_str, duration->value());
+} else {
+strcpy(dur_str,"300");
+}
+
+if (timestep->value()) {
+strcpy(timestep_str, fuel_h->value());
+} else {
+strcpy(timestep_str,"300");
+}
+
+if (in_file->value() > 0) {
+strcpy(input_str,in_file->text(in_file->value()));
+} else {
+strcpy(input_str,new_file->value());
+}
+
+if (mesh_file->value() > 0) {
+strcpy(mesh_str,mesh_file->text(mesh_file->value()));
+} else {
+strcpy(mesh_str,"default_mesh.e");
+}
+
+std::ofstream outfile(input_str,std::ios::out);
+
+assert(outfile.is_open());
+
+outfile << "[//]" << timestep_str << std::endl;}
+      xywh {527 708 100 25}
+    }
+    Fl_File_Browser in_file {
+      label {Input File Location}
+      xywh {26 105 310 235}
+    }
+    Fl_File_Browser mesh_file {
+      label {Mesh File Location}
+      xywh {26 424 310 235}
+    }
+    Fl_Input new_file {
+      label {New Input File Name}
+      xywh {177 364 161 25}
     }
   }
 }
--- GUI/src/fem_b_gui.h
+++ GUI/src/fem_b_gui.h
@@ -4,8 +4,36 @@
 #define fem_b_gui_h
 #include <FL/Fl.H>
 #include <FL/Fl_Double_Window.H>
+extern Fl_Double_Window *main_window;
 #include <FL/Fl_Group.H>
+extern Fl_Group *bc_set;
 #include <FL/Fl_Round_Button.H>
+extern Fl_Round_Button *iso_button;
+extern Fl_Round_Button *adia_button;
 #include <FL/Fl_Input.H>
+extern Fl_Input *wall_temp;
+extern Fl_Input *in_temp;
+extern Fl_Input *out_p;
+extern Fl_Group *gas_set;
+extern Fl_Input *gas_tc;
+extern Fl_Input *gas_dens;
+extern Fl_Input *gas_sh;
+extern Fl_Input *gas_visc;
+extern Fl_Input *gas_mf;
+extern Fl_Group *chem_set;
+extern Fl_Input *rx_rate;
+extern Fl_Input *fuel_h;
+extern Fl_Group *mesh_set;
+extern Fl_Input *x_elem;
+extern Fl_Input *y_elem;
+extern Fl_Group *time_set;
+extern Fl_Input *duration;
+extern Fl_Input *timestep;
+#include <FL/Fl_Button.H>
+extern Fl_Button *submit;
+#include <FL/Fl_File_Browser.H>
+extern Fl_File_Browser *in_file;
+extern Fl_File_Browser *mesh_file;
+extern Fl_Input *new_file;
 Fl_Double_Window* make_window();
 #endif
