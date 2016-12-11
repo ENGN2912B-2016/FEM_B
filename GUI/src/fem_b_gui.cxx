7744f7c28a8b43424471565bc190aea5f820be85
Matthew Petersen <matthew_petersen@brown.edu>
2016-12-11 17:10:30 -0500

    Added error handling and updated input generation

--- GUI/src/fem_b_gui.cxx
+++ GUI/src/fem_b_gui.cxx
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <fstream>
 #include <string>
+#include <FL/fl_ask.H>
 
 /**
  Function to print out input sub-block
@@ -69,7 +70,13 @@ Fl_Input *wall_temp_b=(Fl_Input *)0;
 
 Fl_Input *in_temp=(Fl_Input *)0;
 
-Fl_Input *out_p=(Fl_Input *)0;
+Fl_Input *funcin=(Fl_Input *)0;
+
+Fl_Input *conv_x=(Fl_Input *)0;
+
+Fl_Input *conv_y=(Fl_Input *)0;
+
+Fl_Input *tcoef=(Fl_Input *)0;
 
 Fl_Group *gas_set=(Fl_Group *)0;
 
@@ -80,14 +87,6 @@ Fl_Input *gas_dens=(Fl_Input *)0;
 Fl_Input *gas_sh=(Fl_Input *)0;
 
 Fl_Input *gas_visc=(Fl_Input *)0;
-
-Fl_Input *gas_mf=(Fl_Input *)0;
-
-Fl_Group *chem_set=(Fl_Group *)0;
-
-Fl_Input *rx_rate=(Fl_Input *)0;
-
-Fl_Input *fuel_h=(Fl_Input *)0;
 
 Fl_Group *mesh_set=(Fl_Group *)0;
 
@@ -108,10 +107,20 @@ Fl_Input *timestep=(Fl_Input *)0;
 Fl_Button *submit=(Fl_Button *)0;
 
 static void cb_submit(Fl_Button*, void*) {
-  std::string Twt_str, Twb_str, Ti_str, TC_str, dens_str, SH_str, visc_str, MF_str; //Po_str
+  std::string Twt_str, Twb_str, Ti_str, TC_str, dens_str, SH_str, visc_str, MF_str, func_str, conv_x_str, conv_y_str, tcoef_str; //Po_str
 std::string RX_str, Hfuel_str, El_x_str, El_y_str, dur_str, timestep_str, xmax_str, ymax_str;
 std::string input_str, mesh_str;
 bool adia_iso_t, adia_iso_b;
+
+if (bool(wall_temp_t->value())&&bool(wall_temp_b->value())) {
+if (std::stof(wall_temp_t->value())<0||std::stof(wall_temp_b->value()) < 0) {
+fl_alert("Temperature must be non-negative.");
+}}
+
+if (bool(x_elem->value())&&bool(y_elem->value())) {
+if (std::stof(x_elem->value()) <0 || std::stof(y_elem->value()) < 0) {
+fl_alert("Number of elements must be non-negative.");
+}}
 
 if (iso_button_t->value()) {
 adia_iso_t = false;
@@ -139,6 +148,18 @@ if (wall_temp_b->value()) {
 Twb_str = std::string(wall_temp_b->value());
 } else {
 Twb_str="300";
+}
+
+if (conv_x->value()) {
+conv_x_str = std::string(conv_x->value());
+} else {
+conv_x_str="1.0";
+}
+
+if (conv_y->value()) {
+conv_y_str = std::string(conv_y->value());
+} else {
+conv_y_str="0.0";
 }
 
 if (in_temp->value()) {
@@ -171,52 +192,46 @@ SH_str = std::string("'")+std::string( gas_sh->value())+std::string("'");
 SH_str = std::string("300");
 }
 
+if (funcin->value()) {
+func_str = std::string(funcin->value());
+} else {
+func_str = 1;
+}
+
 if (gas_visc->value()) {
 visc_str = std::string("'")+std::string( gas_visc->value())+std::string("'");
 } else {
 visc_str = std::string("300");
 }
 
-if (gas_mf->value()) {
-MF_str = std::string( gas_mf->value());
-} else {
-MF_str = std::string("300");
-}
-
-if (rx_rate->value()) {
-RX_str = std::string( rx_rate->value());
-} else {
-RX_str = std::string("300");
-}
-
-if (fuel_h->value()) {
-Hfuel_str = std::string( fuel_h->value());
-} else {
-Hfuel_str = std::string("300");
-}
-
 if (x_elem->value()) {
 El_x_str = std::string( x_elem->value());
 } else {
-El_x_str = std::string("300");
+El_x_str = std::string("20");
 }
 
 if (y_elem->value()) {
 El_y_str = std::string( y_elem->value());
 } else {
-El_x_str = std::string("300");
+El_x_str = std::string("20");
 }
 
 if (xmax->value()) {
 xmax_str = std::string( xmax->value());
 } else {
-xmax_str = std::string("300");
+xmax_str = std::string("40");
+}
+
+if (tcoef->value()) {
+tcoef_str = std::string( tcoef->value());
+} else {
+tcoef_str = std::string("1.0");
 }
 
 if (ymax->value()) {
 ymax_str = std::string( ymax->value());
 } else {
-ymax_str = std::string("300");
+ymax_str = std::string("40");
 }
 
 
@@ -229,15 +244,15 @@ dur_str = std::string("300");
 if (timestep->value()) {
 timestep_str = std::string(timestep->value());
 } else {
-timestep_str = std::string("300");
+timestep_str = std::string("2");
 }
+
+input_str = std::string("default.i");
 
 if (in_file->value() > 0) {
 input_str = std::string(in_file->text(in_file->value()));
 } else if (new_file->value()) {
 input_str = std::string(new_file->value());
-} else {
-input_str = std::string("default.i");
 }
 
 if (mesh_file->value() > 0) {
@@ -270,11 +285,21 @@ if (mesh_file->value() == 0 && bool(x_elem->value())&& bool(x_elem->value())) {
 
 outfile << "[]\n\n";
 
-outfile << "[Variables]\n[./temp]\n[../]\n[]\n\n";
+outfile << "[Variables]\n[./temp]\n[../]\n[./convdiff]\norder = FIRST\nfamily = LAGRANGE\n[../]\n[]\n\n";
+
+outfile << "[Functions]\n[./forcing_func]\ntype = ParsedFunction\nvalue = " << func_str << "\n[../]\n[]\n\n";
 
 outfile << "[Kernels]\n[./hc]\n type = HeatConduction\n variable = temp\n[../]\n\n";
 
-outfile << "[./HeatTdot]\ntype = HeatConductionTimeDerivative\nvariable = temp\n[../]\n[]\n\n";
+outfile << "[./HeatTdot]\ntype = HeatConductionTimeDerivative\nvariable = temp\n[../]\n";
+
+outfile << "[./conv]\ntype = Convection\nvariable = convdiff\nvelocity = '" << conv_x_str << " " << conv_y_str << " 0.0'\n[../]\n";
+
+outfile << "[./diff]\ntype = MyDiffusion\nvariable = convdiff\n[../]\n";
+
+outfile << "[./euler]\ntype = MyTimeDerivative\nvariable = convdiff\ntime_coefficient = " << tcoef_str << "\n[../]\n";
+
+outfile << "[./forcing]\ntype = UserForcingFunction\nvariable = temp\nfunction = forcing_func\n[../]\n[]\n\n";
 
 outfile << "[BCs]\n";
 
@@ -283,6 +308,9 @@ std::string dirtop[4] = {std::string("DirichletBC"),std::string("temp"),std::str
 std::string dirleft[4] = {std::string("DirichletBC"),std::string("temp"),std::string("left"),Ti_str};
 std::string dirbot[4] = {std::string("DirichletBC"),std::string("temp"),std::string("bottom"),Twb_str};
 
+std::string dir_diff_left[4] = {std::string("DirichletBC"),std::string("temp"),std::string("left"),std::string("1.0")};
+std::string dir_diff_top[4] = {std::string("DirichletBC"),std::string("temp"),std::string("top"),std::string("0.0")};
+std::string dir_diff_bot[4] = {std::string("DirichletBC"),std::string("temp"),std::string("bottom"),std::string("0.0")};
 
 std::string neunames[4] = {std::string("type"),std::string("variable"),std::string("boundary")};
 std::string neutop[4] = {std::string("NeumannBC"),std::string("temp"),std::string("top")};
@@ -312,6 +340,9 @@ inblock(std::string("bottom"),4,dirnames,dirbot,outfile);
 inblock(std::string("right"),3,neunames,neurig,outfile);
 }
 
+inblock(std::string("left_diffused"),4,dirnames,dir_diff_left,outfile);
+inblock(std::string("top_diffused"),4,dirnames,dir_diff_top,outfile);
+inblock(std::string("bott_diffused"),4,dirnames,dir_diff_bot,outfile);
 
 outfile << "[]\n\n";
 
@@ -343,25 +374,25 @@ Fl_Input *new_file=(Fl_Input *)0;
 
 Fl_Double_Window* make_window() {
   { main_window = new Fl_Double_Window(678, 749, "Laminar Combustion Input File Generator");
-    { bc_set = new Fl_Group(190, 30, 475, 475, "Boundary Conditions");
-      { iso_button_t = new Fl_Round_Button(510, 145, 25, 25, "Isothermal (top");
+    { bc_set = new Fl_Group(190, 30, 475, 475, "Simulation Parameters");
+      { iso_button_t = new Fl_Round_Button(510, 119, 25, 25, "Isothermal (top");
         iso_button_t->down_box(FL_ROUND_DOWN_BOX);
         iso_button_t->callback((Fl_Callback*)cb_iso_button_t);
         iso_button_t->align(Fl_Align(FL_ALIGN_LEFT));
         iso_button_t->when(FL_WHEN_CHANGED);
       } // Fl_Round_Button* iso_button_t
-      { adia_button_t = new Fl_Round_Button(510, 164, 25, 25, "Adiabatic (top)");
+      { adia_button_t = new Fl_Round_Button(510, 138, 25, 25, "Adiabatic (top)");
         adia_button_t->down_box(FL_ROUND_DOWN_BOX);
         adia_button_t->callback((Fl_Callback*)cb_adia_button_t);
         adia_button_t->align(Fl_Align(FL_ALIGN_LEFT));
       } // Fl_Round_Button* adia_button_t
-      { iso_button_b = new Fl_Round_Button(510, 182, 25, 25, "Isothermal (Bottom)");
+      { iso_button_b = new Fl_Round_Button(510, 156, 25, 25, "Isothermal (Bottom)");
         iso_button_b->down_box(FL_ROUND_DOWN_BOX);
         iso_button_b->callback((Fl_Callback*)cb_iso_button_b);
         iso_button_b->align(Fl_Align(FL_ALIGN_LEFT));
         iso_button_b->when(FL_WHEN_CHANGED);
       } // Fl_Round_Button* iso_button_b
-      { adia_button_b = new Fl_Round_Button(510, 199, 25, 25, "Adiabatic (bottom)");
+      { adia_button_b = new Fl_Round_Button(510, 173, 25, 25, "Adiabatic (bottom)");
         adia_button_b->down_box(FL_ROUND_DOWN_BOX);
         adia_button_b->callback((Fl_Callback*)cb_adia_button_b);
         adia_button_b->align(Fl_Align(FL_ALIGN_LEFT));
@@ -372,45 +403,42 @@ Fl_Double_Window* make_window() {
       } // Fl_Input* wall_temp_b
       { in_temp = new Fl_Input(510, 95, 130, 25, "Inlet Temperature");
       } // Fl_Input* in_temp
-      { out_p = new Fl_Input(510, 120, 130, 25, "Outlet Pressure");
-      } // Fl_Input* out_p
+      { funcin = new Fl_Input(510, 197, 130, 25, "Temp. Forcing Function");
+      } // Fl_Input* funcin
+      { conv_x = new Fl_Input(510, 220, 130, 25, "Conv. X coefficient");
+      } // Fl_Input* conv_x
+      { conv_y = new Fl_Input(510, 245, 130, 25, "Conv. Y coefficient");
+      } // Fl_Input* conv_y
+      { tcoef = new Fl_Input(510, 270, 130, 25, "Time coefficient");
+      } // Fl_Input* tcoef
       bc_set->end();
     } // Fl_Group* bc_set
-    { gas_set = new Fl_Group(185, 257, 475, 473, "Gas Settings");
-      { gas_tc = new Fl_Input(510, 266, 130, 25, "Thermal Conductivity");
+    { gas_set = new Fl_Group(185, 323, 475, 473, "Gas Settings");
+      { gas_tc = new Fl_Input(510, 332, 130, 25, "Thermal Conductivity");
       } // Fl_Input* gas_tc
-      { gas_dens = new Fl_Input(510, 290, 130, 25, "Density");
+      { gas_dens = new Fl_Input(510, 356, 130, 25, "Density");
       } // Fl_Input* gas_dens
-      { gas_sh = new Fl_Input(510, 315, 130, 25, "Specific Heat");
+      { gas_sh = new Fl_Input(510, 381, 130, 25, "Specific Heat");
       } // Fl_Input* gas_sh
-      { gas_visc = new Fl_Input(510, 340, 130, 25, "Viscosity");
+      { gas_visc = new Fl_Input(510, 406, 130, 25, "Viscosity");
       } // Fl_Input* gas_visc
-      { gas_mf = new Fl_Input(510, 365, 130, 25, "Percent Fuel");
-      } // Fl_Input* gas_mf
       gas_set->end();
     } // Fl_Group* gas_set
-    { chem_set = new Fl_Group(185, 433, 475, 477, "Chemistry Settings");
-      { rx_rate = new Fl_Input(510, 442, 130, 25, "Reaction Rate");
-      } // Fl_Input* rx_rate
-      { fuel_h = new Fl_Input(510, 464, 130, 25, "Enthalpy of Fuel");
-      } // Fl_Input* fuel_h
-      chem_set->end();
-    } // Fl_Group* chem_set
-    { mesh_set = new Fl_Group(185, 527, 475, 475, "Mesh Settings");
-      { x_elem = new Fl_Input(510, 542, 130, 25, "Elements (X)");
+    { mesh_set = new Fl_Group(185, 460, 475, 475, "Mesh Settings");
+      { x_elem = new Fl_Input(510, 468, 130, 25, "Elements (X)");
       } // Fl_Input* x_elem
-      { y_elem = new Fl_Input(510, 567, 130, 25, "Elements (Y)");
+      { y_elem = new Fl_Input(510, 493, 130, 25, "Elements (Y)");
       } // Fl_Input* y_elem
-      { xmax = new Fl_Input(510, 592, 130, 25, "X Max");
+      { xmax = new Fl_Input(510, 518, 130, 25, "X Max");
       } // Fl_Input* xmax
-      { ymax = new Fl_Input(510, 617, 130, 25, "Y Max");
+      { ymax = new Fl_Input(510, 543, 130, 25, "Y Max");
       } // Fl_Input* ymax
       mesh_set->end();
     } // Fl_Group* mesh_set
     { time_set = new Fl_Group(185, 657, 475, 675, "Time Settings");
-      { duration = new Fl_Input(510, 682, 130, 25, "Number of Steps");
+      { duration = new Fl_Input(510, 666, 130, 25, "Number of Steps");
       } // Fl_Input* duration
-      { timestep = new Fl_Input(510, 707, 130, 25, "Timestep");
+      { timestep = new Fl_Input(510, 691, 130, 25, "Timestep");
       } // Fl_Input* timestep
       time_set->end();
     } // Fl_Group* time_set
--- GUI/src/fem_b_gui.f
+++ GUI/src/fem_b_gui.f
@@ -14,9 +14,12 @@ decl {\#include <fstream>} {private local
 decl {\#include <string>} {private local
 }
 
+decl {\#include <FL/fl_ask.H>} {private local
+}
+
 Function {inblock(std::string name, int size, std::string ent[], std::string val[],std::ostream& out)} {
   comment {Function to print out input sub-block
-} open selected return_type {std::ostream&}
+} open return_type {std::ostream&}
 } {
   code {out << "[./" << name << "]\\n";
 if (size > 0) {
@@ -32,10 +35,10 @@ Function {make_window()} {open
 } {
   Fl_Window main_window {
     label {Laminar Combustion Input File Generator} open
-    xywh {185 141 678 749} type Double visible
+    xywh {1089 296 678 749} type Double visible
   } {
     Fl_Group bc_set {
-      label {Boundary Conditions} open
+      label {Simulation Parameters} open
       xywh {190 30 475 475}
     } {
       Fl_Round_Button iso_button_t {
@@ -45,7 +48,7 @@ adia_button_t->value(0);
 } else {
 adia_button_t->value(1);
 }}
-        xywh {510 145 25 25} down_box ROUND_DOWN_BOX align 4 when 1
+        xywh {510 119 25 25} down_box ROUND_DOWN_BOX align 4 when 1
       }
       Fl_Round_Button adia_button_t {
         label {Adiabatic (top)}
@@ -54,7 +57,7 @@ iso_button_t->value(0);
 } else {
 iso_button_t->value(1);
 }}
-        xywh {510 164 25 25} down_box ROUND_DOWN_BOX align 4
+        xywh {510 138 25 25} down_box ROUND_DOWN_BOX align 4
       }
       Fl_Round_Button iso_button_b {
         label {Isothermal (Bottom)}
@@ -63,7 +66,7 @@ adia_button_b->value(0);
 } else {
 adia_button_b->value(1);
 }}
-        xywh {510 182 25 25} down_box ROUND_DOWN_BOX align 4 when 1
+        xywh {510 156 25 25} down_box ROUND_DOWN_BOX align 4 when 1
       }
       Fl_Round_Button adia_button_b {
         label {Adiabatic (bottom)}
@@ -72,7 +75,7 @@ iso_button_b->value(0);
 } else {
 iso_button_b->value(1);
 }}
-        xywh {510 199 25 25} down_box ROUND_DOWN_BOX align 4
+        xywh {510 173 25 25} down_box ROUND_DOWN_BOX align 4
       }
       Fl_Input wall_temp_t {
         label {Wall Temperature (top)}
@@ -86,68 +89,63 @@ iso_button_b->value(1);
         label {Inlet Temperature}
         xywh {510 95 130 25}
       }
-      Fl_Input out_p {
-        label {Outlet Pressure}
-        xywh {510 120 130 25}
+      Fl_Input funcin {
+        label {Temp. Forcing Function}
+        xywh {510 197 130 25}
+      }
+      Fl_Input conv_x {
+        label {Conv. X coefficient}
+        xywh {510 220 130 25}
+      }
+      Fl_Input conv_y {
+        label {Conv. Y coefficient}
+        xywh {510 245 130 25}
+      }
+      Fl_Input tcoef {
+        label {Time coefficient}
+        xywh {510 270 130 25}
       }
     }
     Fl_Group gas_set {
       label {Gas Settings} open
-      xywh {185 257 475 473}
+      xywh {185 323 475 473}
     } {
       Fl_Input gas_tc {
         label {Thermal Conductivity}
-        xywh {510 266 130 25}
+        xywh {510 332 130 25}
       }
       Fl_Input gas_dens {
         label Density
-        xywh {510 290 130 25}
+        xywh {510 356 130 25}
       }
       Fl_Input gas_sh {
         label {Specific Heat}
-        xywh {510 315 130 25}
+        xywh {510 381 130 25}
       }
       Fl_Input gas_visc {
         label Viscosity
-        xywh {510 340 130 25}
-      }
-      Fl_Input gas_mf {
-        label {Percent Fuel}
-        xywh {510 365 130 25}
-      }
-    }
-    Fl_Group chem_set {
-      label {Chemistry Settings} open
-      xywh {185 433 475 477}
-    } {
-      Fl_Input rx_rate {
-        label {Reaction Rate}
-        xywh {510 442 130 25}
-      }
-      Fl_Input fuel_h {
-        label {Enthalpy of Fuel}
-        xywh {510 464 130 25}
+        xywh {510 406 130 25}
       }
     }
     Fl_Group mesh_set {
       label {Mesh Settings} open
-      xywh {185 527 475 475}
+      xywh {185 460 475 475}
     } {
       Fl_Input x_elem {
         label {Elements (X)}
-        xywh {510 535 130 25}
+        xywh {510 468 130 25}
       }
       Fl_Input y_elem {
         label {Elements (Y)}
-        xywh {510 560 130 25}
+        xywh {510 493 130 25}
       }
       Fl_Input xmax {
         label {X Max}
-        xywh {510 585 130 25}
+        xywh {510 518 130 25}
       }
       Fl_Input ymax {
         label {Y Max}
-        xywh {510 610 130 25}
+        xywh {510 543 130 25}
       }
     }
     Fl_Group time_set {
@@ -165,10 +163,20 @@ iso_button_b->value(1);
     }
     Fl_Button submit {
       label Submit
-      callback {std::string Twt_str, Twb_str, Ti_str, TC_str, dens_str, SH_str, visc_str, MF_str; //Po_str
+      callback {std::string Twt_str, Twb_str, Ti_str, TC_str, dens_str, SH_str, visc_str, MF_str, func_str, conv_x_str, conv_y_str, tcoef_str; //Po_str
 std::string RX_str, Hfuel_str, El_x_str, El_y_str, dur_str, timestep_str, xmax_str, ymax_str;
 std::string input_str, mesh_str;
 bool adia_iso_t, adia_iso_b;
+
+if (bool(wall_temp_t->value())&&bool(wall_temp_b->value())) {
+if (std::stof(wall_temp_t->value())<0||std::stof(wall_temp_b->value()) < 0) {
+fl_alert("Temperature must be non-negative.");
+}}
+
+if (bool(x_elem->value())&&bool(y_elem->value())) {
+if (std::stof(x_elem->value()) <0 || std::stof(y_elem->value()) < 0) {
+fl_alert("Number of elements must be non-negative.");
+}}
 
 if (iso_button_t->value()) {
 adia_iso_t = false;
@@ -196,6 +204,18 @@ if (wall_temp_b->value()) {
 Twb_str = std::string(wall_temp_b->value());
 } else {
 Twb_str="300";
+}
+
+if (conv_x->value()) {
+conv_x_str = std::string(conv_x->value());
+} else {
+conv_x_str="1.0";
+}
+
+if (conv_y->value()) {
+conv_y_str = std::string(conv_y->value());
+} else {
+conv_y_str="0.0";
 }
 
 if (in_temp->value()) {
@@ -228,52 +248,46 @@ SH_str = std::string("'")+std::string( gas_sh->value())+std::string("'");
 SH_str = std::string("300");
 }
 
+if (funcin->value()) {
+func_str = std::string(funcin->value());
+} else {
+func_str = 1;
+}
+
 if (gas_visc->value()) {
 visc_str = std::string("'")+std::string( gas_visc->value())+std::string("'");
 } else {
 visc_str = std::string("300");
 }
 
-if (gas_mf->value()) {
-MF_str = std::string( gas_mf->value());
-} else {
-MF_str = std::string("300");
-}
-
-if (rx_rate->value()) {
-RX_str = std::string( rx_rate->value());
-} else {
-RX_str = std::string("300");
-}
-
-if (fuel_h->value()) {
-Hfuel_str = std::string( fuel_h->value());
-} else {
-Hfuel_str = std::string("300");
-}
-
 if (x_elem->value()) {
 El_x_str = std::string( x_elem->value());
 } else {
-El_x_str = std::string("300");
+El_x_str = std::string("20");
 }
 
 if (y_elem->value()) {
 El_y_str = std::string( y_elem->value());
 } else {
-El_x_str = std::string("300");
+El_x_str = std::string("20");
 }
 
 if (xmax->value()) {
 xmax_str = std::string( xmax->value());
 } else {
-xmax_str = std::string("300");
+xmax_str = std::string("40");
+}
+
+if (tcoef->value()) {
+tcoef_str = std::string( tcoef->value());
+} else {
+tcoef_str = std::string("1.0");
 }
 
 if (ymax->value()) {
 ymax_str = std::string( ymax->value());
 } else {
-ymax_str = std::string("300");
+ymax_str = std::string("40");
 }
 
 
@@ -286,15 +300,15 @@ dur_str = std::string("300");
 if (timestep->value()) {
 timestep_str = std::string(timestep->value());
 } else {
-timestep_str = std::string("300");
+timestep_str = std::string("2");
 }
+
+input_str = std::string("default.i");
 
 if (in_file->value() > 0) {
 input_str = std::string(in_file->text(in_file->value()));
 } else if (new_file->value()) {
 input_str = std::string(new_file->value());
-} else {
-input_str = std::string("default.i");
 }
 
 if (mesh_file->value() > 0) {
@@ -327,11 +341,21 @@ if (mesh_file->value() == 0 && bool(x_elem->value())&& bool(x_elem->value())) {
 
 outfile << "[]\\n\\n";
 
-outfile << "[Variables]\\n[./temp]\\n[../]\\n[]\\n\\n";
+outfile << "[Variables]\\n[./temp]\\n[../]\\n[./convdiff]\\norder = FIRST\\nfamily = LAGRANGE\\n[../]\\n[]\\n\\n";
+
+outfile << "[Functions]\\n[./forcing_func]\\ntype = ParsedFunction\\nvalue = " << func_str << "\\n[../]\\n[]\\n\\n";
 
 outfile << "[Kernels]\\n[./hc]\\n type = HeatConduction\\n variable = temp\\n[../]\\n\\n";
 
-outfile << "[./HeatTdot]\\ntype = HeatConductionTimeDerivative\\nvariable = temp\\n[../]\\n[]\\n\\n";
+outfile << "[./HeatTdot]\\ntype = HeatConductionTimeDerivative\\nvariable = temp\\n[../]\\n";
+
+outfile << "[./conv]\\ntype = Convection\\nvariable = convdiff\\nvelocity = '" << conv_x_str << " " << conv_y_str << " 0.0'\\n[../]\\n";
+
+outfile << "[./diff]\\ntype = MyDiffusion\\nvariable = convdiff\\n[../]\\n";
+
+outfile << "[./euler]\\ntype = MyTimeDerivative\\nvariable = convdiff\\ntime_coefficient = " << tcoef_str << "\\n[../]\\n";
+
+outfile << "[./forcing]\\ntype = UserForcingFunction\\nvariable = temp\\nfunction = forcing_func\\n[../]\\n[]\\n\\n";
 
 outfile << "[BCs]\\n";
 
@@ -340,6 +364,9 @@ std::string dirtop[4] = {std::string("DirichletBC"),std::string("temp"),std::str
 std::string dirleft[4] = {std::string("DirichletBC"),std::string("temp"),std::string("left"),Ti_str};
 std::string dirbot[4] = {std::string("DirichletBC"),std::string("temp"),std::string("bottom"),Twb_str};
 
+std::string dir_diff_left[4] = {std::string("DirichletBC"),std::string("temp"),std::string("left"),std::string("1.0")};
+std::string dir_diff_top[4] = {std::string("DirichletBC"),std::string("temp"),std::string("top"),std::string("0.0")};
+std::string dir_diff_bot[4] = {std::string("DirichletBC"),std::string("temp"),std::string("bottom"),std::string("0.0")};
 
 std::string neunames[4] = {std::string("type"),std::string("variable"),std::string("boundary")};
 std::string neutop[4] = {std::string("NeumannBC"),std::string("temp"),std::string("top")};
@@ -369,6 +396,9 @@ inblock(std::string("bottom"),4,dirnames,dirbot,outfile);
 inblock(std::string("right"),3,neunames,neurig,outfile);
 }
 
+inblock(std::string("left_diffused"),4,dirnames,dir_diff_left,outfile);
+inblock(std::string("top_diffused"),4,dirnames,dir_diff_top,outfile);
+inblock(std::string("bott_diffused"),4,dirnames,dir_diff_bot,outfile);
 
 outfile << "[]\\n\\n";
 
@@ -389,7 +419,7 @@ outfile << "num_steps = " << dur_str << "\\n";
 outfile << "dt = " << timestep_str << "\\n";
 outfile << "[]\\n\\n";
 
-outfile << "[Outputs]\\nexecute_on = 'timestep_end'\\nexodus = true\\n[]";}
+outfile << "[Outputs]\\nexecute_on = 'timestep_end'\\nexodus = true\\n[]";} selected
       xywh {230 705 100 25}
     }
     Fl_File_Browser in_file {
